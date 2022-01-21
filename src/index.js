537ee6c1ccb66bcd628c237f96392dd512c41d86
Salohiddin98 <salohiddin.rashidov98@gmail.com>
2022-01-21 22:48:45 +0500

    refactor:add solution of task in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let str = n.toString();
+   let str1 = '';
+   for (let i = str.length; i > 0; i--) {
+      str1 += str.charAt(i - 1);
+   }
+   str = str1.replace('-', '');
+   return str;
 }
